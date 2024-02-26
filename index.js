6b389ddd01ef7fb9ca4b098800c208474f60489f
jeyabdo <joephoenix755@gmail.com>
2024-02-27 02:10:26 +0530

    added package for inquirer

--- index.js
+++ index.js
@@ -2,61 +2,68 @@ const inquirer = require('inquirer');
 const markdownIt = require('markdown-it');
 const fs = require('fs');
 
-
+const md = new markdownIt();
 
 inquirer
-.prompt([
-    {
-        type: 'input',
-        name: 'nameOfProject',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'checkbox',
-        message: 'Table of Contents',
-        name: 'content',
-        choices: ['Description', 'Installation', 'Usage', 'License', 'Contributing'],
-    },
-    {
-        type: 'input',
-        name: 'Description',
-        message: 'Provide a brief description of your project:',
-    },
-    {
-        type: 'input',
-        name: 'Installation',
-        message: 'How should users install your project?',
-    },
-    {
-        type: 'input',
-        name: 'Usage',
-        message: 'How can users use your project?',
-    },
-    {
-        type: 'input',
-        name: 'License',
-        message: 'Specify the license for your project (e.g., MIT):',
-        default: 'MIT',
-    },
-    {
-        type: 'input',
-        name: 'Contributing',
-        message: 'How can others contribute to your project?',
-    },
+    .prompt([
+        {
+            type: 'input',
+            name: 'nameOfProject',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'checkbox',
+            message: 'Table of Contents',
+            name: 'content',
+            choices: ['Description', 'Installation', 'Usage', 'License', 'Contributing'],
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Provide a brief description of your project:',
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'How should users install your project?',
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'How can users use your project?',
+        },
+        {
+            type: 'input',
+            name: 'License',
+            message: 'Specify the license for your project (e.g., MIT):',
+            default: 'MIT',
+        },
+        {
+            type: 'input',
+            name: 'Contributing',
+            message: 'How can others contribute to your project?',
+        },
+    ])
+    .then((data) => {
+        // Construct Markdown content
+        let markdownContent = `# ${data.nameOfProject}\n\n`;
 
+        // Add selected sections to the table of contents
+        data.content.forEach((section) => {
+            markdownContent += `## ${section}\n\n`;
+            markdownContent += `${data[section]}\n\n`;
+        });
 
-
-]).then((data) => {
-    
-    
-    fs.writeFile("README.md", JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
-   
-
-
-    
-}).catch((err) => {
-    
-});
+        // Write Markdown content to README.md file
+        fs.writeFile("README.md", markdownContent, (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('README.md file generated successfully!');
+            }
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
